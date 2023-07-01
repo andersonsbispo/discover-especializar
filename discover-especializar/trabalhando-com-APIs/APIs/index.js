f68d844f8897a9e8b6e78c27f4844cea752d0d1e
Anderson Bispo <andersonsbispo1997@outlook.com>
2023-06-30 22:53:48 -0300

    Trabalhando com APIs - Seção 2

---
+++ discover-especializar/trabalhando-com-APIs/APIs/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+
+app.listen("3000");
+
+let author = "Anderson"
+
+//middleware
+app.use(express.json());
+
+//Get
+app.route("/").get((req, res) => res.send(author));
+
+//Post
+app.route("/").post((req, res) => res.send(req.body));
+
+//Put
+app.route("/").put((req, res) => {
+    author = req.body.author;
+    res.send(author);
+});
+
+//Delete
+app.route("/:identificador").delete((req, res) => {
+    res.send(req.params.identificador);
+});
